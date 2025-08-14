b40ea793647426ff37829beb560d35bcbe89bfe7
DaviSantosDev77 <davi.flafla@gmail.com>
2025-08-14 11:25:41 -0300

    Adiciona projeto de tabuada de multiplicação.

---
+++ Tabuada_multiplicação.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+ 
+   int main() {
+
+      int nro, linha;
+      for(nro = 1; nro < 10; nro ++){
+
+         printf("\n----Tabuada do %d----/n", nro, linha);
+
+      for(linha = 1; linha <=10; linha ++){
+
+         printf("%d x %d = %d\n", nro, linha, nro*linha);
+
+      printf(************\n\n);
+
+      }
+
+    return 0;
+  }
+
+ }
+
